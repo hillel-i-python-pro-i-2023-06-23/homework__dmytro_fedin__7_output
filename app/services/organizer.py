83c91b225851e3fb39202d2a7ab6fc93aadebee8
dafedin <tm.i.sm.master@gmail.com>
2023-07-18 16:27:55 +0300

    defaultDict alias added as T_Group

--- app/services/organizer.py
+++ app/services/organizer.py
@@ -1,8 +1,13 @@
+import collections
 from collections import defaultdict
+from typing import TypeAlias
+
 from app.services.human import T_HUMANS
 
+T_GROUP: TypeAlias = collections.defaultdict[list[str]]
 
-def organize_data(humans: T_HUMANS):
+
+def organize_data(humans: T_HUMANS) -> T_GROUP:
     """
     Use default-dict object to organized data
     """
